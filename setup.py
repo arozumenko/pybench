e8121db32fb14f30be2aac1d4ca291207fbda1d9
arozumenko <artyom.rozumenko@gmail.com>
2016-07-11 14:43:29 +0300

    added initial code

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!env python
+from setuptools import setup
+
+INSTALL_REQUIREMENTS = [
+    'argparse'
+]
+
+VERSION = '1.0.0a'
+
+setup(name='pybench',
+      version=VERSION,
+      packages=[
+          'pybench',
+      ],
+      install_requires=INSTALL_REQUIREMENTS,
+      include_package_data=True,
+      # Metadata.
+      description='lightweight HTTP/HTTPS benchmark tool written on Python',
+      author='arozumenko',
+      entry_points={
+          'console_scripts': ['pybench=pybench.pybench:main'],
+      },
+      author_email='artem.rozumenko@gmail.com',
+      url='http://none',
+      license='MIT')
