9f4a71e885516bd48cb941c7807cc58489b8be6d
arozumenko <artyom.rozumenko@gmail.com>
2016-07-11 15:43:14 +0300

    getting ready to PyPi

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='webbench',
           'webbench',
       ],
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
